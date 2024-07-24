7cf022214371572afdbb65e3376ce7d3e9e5a829
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-23 21:13:33 -0700

    added the access giver for currentUser and outside User

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -12,9 +12,8 @@ import Image from 'next/image'
 import { updateDocument } from '@/lib/actions/room.actions'
 import Loader from './Loader'
 
-const CollaborativeRoom = ({roomId, roomMetadata} : CollaborativeRoomProps) => {
+const CollaborativeRoom = ({roomId, roomMetadata, users, currentUserType} : CollaborativeRoomProps) => {
   
-  const currentUserType = 'editor'
 
   const[documentTitle, setDocumentTitle] = useState(roomMetadata.title)
   const[editing, setEditing] = useState(false)
@@ -116,7 +115,7 @@ const CollaborativeRoom = ({roomId, roomMetadata} : CollaborativeRoomProps) => {
             </div>
             
           </Header>
-          <Editor/>
+          <Editor roomId={roomId} currentUserType={currentUserType}/>
           </div>
         </ClientSideSuspense>
     </RoomProvider>
