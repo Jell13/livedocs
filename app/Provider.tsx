f0cc86ece0009e507ce015528af10b3dfdc6ca2d
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-07-24 14:33:34 -0700

    added the mention function in the text

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -4,8 +4,12 @@ import Loader from "@/components/Loader"
 import { getClerkUsers } from "@/lib/actions/user.actions"
 import { ClientSideSuspense, LiveblocksProvider } from "@liveblocks/react/suspense" 
 import { ReactNode } from "react"
+import { getDocumentUsers } from "@/lib/actions/user.actions"
+import { useUser } from "@clerk/nextjs"
 
 const Provider = ({children} : {children : ReactNode}) => {
+
+  const { user: clerkUser } = useUser() 
   return (
     <LiveblocksProvider 
     authEndpoint={'/api/liveblocks-auth'}
@@ -13,6 +17,15 @@ const Provider = ({children} : {children : ReactNode}) => {
       const users = await getClerkUsers({userIds})
 
       return users
+    }}
+    resolveMentionSuggestions={async ({text, roomId}) =>{
+      const roomUsers = await getDocumentUsers({
+        roomId,
+        currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+        text
+      })
+
+      return roomUsers
     }}
     >
         <ClientSideSuspense fallback={<Loader/>}>
